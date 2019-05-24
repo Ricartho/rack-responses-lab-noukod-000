6afb0ac1c81261bd5a997dc0531e8483e45def27
Ricartho Franck <ricartho.franck@esih.edu>
2019-05-24 15:44:32 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -5,9 +5,9 @@ class Application
     time = Time.new
     #resp.write "#{time}"
     if time.hour < 12 
-      
+      resp.write "Good Morning!"
     else
-     
+     resp.write "Good Afternoon"
     end 
     resp.finish
   end 
